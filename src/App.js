7596c29df94c7ba2b54ec3658175a63875425675
super-3ero <57857348+super-3ero@users.noreply.github.com>
2023-04-18 14:58:59 +0300

    configured  react-router and changed file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="bg-green-500 App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-100">
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {/* add your route here. example below */}
+          {/* <Route path="/signup" element={<Signup />} /> */}
+        </Route>
+      </Routes>
     </div>
   );
 }
