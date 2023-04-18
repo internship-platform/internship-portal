90514eb9672a433e4adc13ab10aab00d65486727
Biruk <biruk.alamirew77@gmail.com>
2023-04-18 22:02:05 +0300

    add: finished internship-card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Box from "@mui/material/Box";
+
 import Navbar from "../components/Navbar";
+import ICard from "../components/ICard";
+
+const cards = ["Programming", "Web Development", "Graphics", "Content-writing"];
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Box
+        sx={{
+          marginLeft: 2,
+          marginTop: 2,
+        }}
+      >
+        {cards.map((card) => (
+          <>
+            <ICard title={card} />
+            <br />
+          </>
+        ))}
+      </Box>
     </>
   );
 };
