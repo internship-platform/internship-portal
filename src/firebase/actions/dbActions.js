c4aa512538e91b73c684863a56e94b0520867782
Bedru umer <bedru2019@gmail.com>
2023-04-18 15:53:09 +0200

    added firebase queries for firestore

---
+++ src/firebase/actions/dbActions.js
@@ -0,0 +1,44 @@
+import { db } from "../firebase";
+
+export const addStudent = (student) => {
+  return db.collection("students").add(student);
+};
+
+export const getAllStudents = () => {
+  return db.collection("students").get();
+};
+
+export const getStudentById = (id) => {
+  return db.collection("students").where("studentId", "==", id).get();
+};
+
+export const addCompany = (company) => {
+  return db.collection("companies").add(company);
+};
+
+export const getAllCompanies = () => {
+  return db.collection("companies").get();
+};
+
+export const getCompanyById = (id) => {
+  return db.collection("companies").where("companyId", "==", id).get();
+};
+
+export const addInternship = (internship) => {
+  return db.collection("internships").add(internship);
+};
+
+export const getAllInternships = () => {
+  return db.collection("internships").get();
+};
+
+export const getInternshipById = (id) => {
+  return db.collection("internships").where("internshipId", "==", id).get();
+};
+
+export const getInternshipsByCompanyId = (companyId) => {
+  return getCompanyById(companyId).then((querySnapshot) => {
+    let company = querySnapshot.docs[0].data();
+    return company.postedInternships;
+  });
+};
