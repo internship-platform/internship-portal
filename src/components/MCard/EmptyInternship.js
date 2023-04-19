9f59fecd98e09d0ef9c26e7825c5398c0119b98a
super-3ero <57857348+super-3ero@users.noreply.github.com>
2023-04-19 13:31:37 +0300

    my Internship

---
+++ src/components/MCard/EmptyInternship.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const EmptyInternship = () => {
+  return (
+    <section className="w-screen h-screen flex justify-center items-center">
+      <div className="bg-gray-200 p-4 rounded-md">
+        <p className="text-gray-800 text-center">
+          You have not applied to any internships !
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default EmptyInternship;
